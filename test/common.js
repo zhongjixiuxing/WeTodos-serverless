d7a3371f3a069615e4e8c76269dcf6481a7acecf
anxing <anxing131@gmail.com>
2019-11-04 09:18:04 +0800

    - Update github action

--- test/common.js
+++ test/common.js
@@ -47,7 +47,7 @@ function startSlsOffline(done) {
     slsEnv.JWT_PRIVATE_KEY = config.JWT_PRIVATE_KEY;
     slsEnv.JWT_PUBLIC_KEY = config.JWT_PUBLIC_KEY;
 
-    slsOfflineProcess = spawn("sudo", ["sls", "offline", "start", '--config', 'serverless.yaml'], {env: slsEnv});
+    slsOfflineProcess = spawn("sls", ["offline", "start", '--config', 'serverless.yaml'], {env: slsEnv});
 
     console.log(`Serverless: Offline started with PID : ${slsOfflineProcess.pid}`);
 
@@ -63,6 +63,7 @@ function startSlsOffline(done) {
         done(errData);
     });
 
+    console.log('syer-------------------')
     return slsOfflineProcess;
 }
 
